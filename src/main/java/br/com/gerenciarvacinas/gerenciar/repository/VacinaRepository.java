68a39815ead020f85db072edfd744c16b3c0675d
Mário Evangelista <121322767+mario-evangelista@users.noreply.github.com>
2023-10-10 21:04:55 -0300

    UPDATE application.properties api-vacinas-db

--- src/main/java/br/com/gerenciarvacinas/gerenciar/repository/VacinaRepository.java
+++ src/main/java/br/com/gerenciarvacinas/gerenciar/repository/VacinaRepository.java
@@ -4,5 +4,5 @@ import br.com.gerenciarvacinas.gerenciar.entities.Vacina;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface VacinaRepository extends MongoRepository<Vacina, String> {
-    Vacina findById(int id);
+    public Vacina findById(int id);
 }
