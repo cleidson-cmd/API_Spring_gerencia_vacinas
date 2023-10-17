fb7e067216f980ecd94a82e32415055fff552240
mariojbe <mariojbe@gmail.com>
2023-10-17 13:04:13 -0300

    SPRING BOOT VALIDATION

--- src/main/java/br/com/gerenciarvacinas/gerenciar/controller/VacinaController.java
+++ src/main/java/br/com/gerenciarvacinas/gerenciar/controller/VacinaController.java
@@ -5,10 +5,15 @@ import br.com.gerenciarvacinas.gerenciar.service.VacinaService;
 import jakarta.validation.Valid;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 @RestController
@@ -17,6 +22,19 @@ public class VacinaController {
 
     @Autowired
     VacinaService vacinaService;
+
+    // Método de Valodação e Exceções
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Map<String, String> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        return errors;
+    }
 
     @GetMapping
     public List<Vacina> obterTodos() {
--- src/main/java/br/com/gerenciarvacinas/gerenciar/entities/Vacina.java
+++ src/main/java/br/com/gerenciarvacinas/gerenciar/entities/Vacina.java
@@ -1,12 +1,10 @@
 package br.com.gerenciarvacinas.gerenciar.entities;
 
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.bson.types.ObjectId;
+import org.hibernate.validator.constraints.ScriptAssert;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -17,21 +15,26 @@ import java.util.Date;
 @AllArgsConstructor //cria construtor com todos argumentos
 @NoArgsConstructor //cria construtor sem argumentos
 @Document(collection = "vacinas")
+//Expressão em JavaScript que será executada durante a validação
+//@ScriptAssert(lang = "javascript", script = "_this.validade.isAfter(LocalDateTime.now())", message = "A data de validade deve ser futura")
 public class Vacina {
     @Id
     private String id;
     @NotNull
     @Indexed(unique = true)
     private int codigo;
-    @NotEmpty
+    @NotBlank(message = "O nome da vacina não foi informado!")
     private String nome;
-    @NotEmpty
+    @NotBlank(message = "O nome do fabricante não foi informado!")
     private String fabricante;
     @NotNull
-    @Min(value = 1)
+    @Min(value = 1, message = "Informe um número maior que 0!")
     private String lote;
     @NotNull
+    @Future(message = "A data de validade deve ser futura!")
     private Date validade;
-    private int doses;
-    private String intervaloEntreDoses;
+    @NotNull
+    @Min(value = 1, message = "Informe um número maior que 0!")
+    private int doses; // Número de doses
+    private String intervaloEntreDoses;// Intervalo mínimo entre doses em dias
 }
