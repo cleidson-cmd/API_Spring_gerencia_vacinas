e3465efeb02ab79854b126f679bfd16bbb0ec94c
mariojbe <mariojbe@gmail.com>
2023-10-17 08:49:59 -0300

    system.properties

--- src/main/java/br/com/gerenciarvacinas/gerenciar/Application.java
+++ src/main/java/br/com/gerenciarvacinas/gerenciar/Application.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
-public class GerenciarApplication {
+public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(GerenciarApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
 
 }
--- src/test/java/br/com/gerenciarvacinas/gerenciar/ApplicationTests.java
+++ src/test/java/br/com/gerenciarvacinas/gerenciar/ApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class GerenciarApplicationTests {
+class ApplicationTests {
 
 	@Test
 	void contextLoads() {
